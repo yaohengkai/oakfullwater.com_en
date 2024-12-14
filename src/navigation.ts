c7892652672b9d60825b62cbbbba6950d422644f
Hengkai <hengkai1993@163.com>
2024-12-14 16:36:47 +0800

    change logo

--- src/navigation.ts
+++ src/navigation.ts
@@ -203,7 +203,7 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/yaohengkai/oakfull.cn_en' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
+    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="/src/assets/favicons/apple-touch-icon.png" alt="onWidget logo" loading="lazy"></img>
     Made by <a class="text-blue-600  dark:text-muted" href="https://oakfull.cn/"> Oakfull</a> · All rights reserved.
   `,
 };
